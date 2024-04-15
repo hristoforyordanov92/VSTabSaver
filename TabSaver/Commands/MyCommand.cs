96baed46bdd356e6ce30fa4690f328bef422cc95
Hristofor Yordanov <hristofor.yordanov.92@gmail.com>
2024-04-15 17:37:21 +0300

    Initial commit of the code

---
+++ TabSaver/Commands/MyCommand.cs
@@ -0,0 +1,117 @@
+﻿using System.Reflection;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace TabSaver
+{
+    [Command(PackageIds.SaveTabsCommand)]
+    internal sealed class SaveTabsCommand : BaseCommand<SaveTabsCommand>
+    {
+        protected override void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE2 dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+
+            Solution solution = new(dte.Solution.FullName);
+
+            foreach (Document document in dte.Documents)
+            {
+                Window activeWindow = document.ActiveWindow;
+
+                // some documents don't have a window. this happens with .xaml documents (maybe other types too).
+                // they are explicitly closed from the X button on the tab, but they are still in the Documents collection
+                if (activeWindow == null)
+                    continue;
+
+                bool isPinned = IsDocumentPinned(document);
+
+                solution.Tabs.Add(new Tab(document.FullName, isPinned));
+            }
+
+            SaveFileManager.Save(solution);
+        }
+
+        /// <summary>
+        /// Looks for a non-public property indicating if the document's window is pinned and returns the result.
+        /// </summary>
+        /// <param name="document">The document for which we want to know if it's pinned or not.</param>
+        /// <returns>True if the document is pinned, otherwise false.</returns>
+        private static bool IsDocumentPinned(Document document)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (document == null)
+                return false;
+
+            // some documents have multiple windows associated with them. idk why
+            // if we use document.ActiveWindow then we might end up with "IsPinned = false", because
+            // the DockViewElement is null for that specific window. so we go over each window
+            // and we get a true/false based on wether at least a single one is true, otherwise false
+            foreach (Window window in document.Windows)
+            {
+                if (window == null)
+                    continue;
+
+                PropertyInfo dockViewElementProperty = window
+                    .GetType()
+                    .GetProperty(
+                        "DockViewElement",
+                        BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty);
+
+                if (dockViewElementProperty == null)
+                    continue;
+
+                object dockViewElementObject = dockViewElementProperty.GetValue(window);
+                if (dockViewElementObject == null)
+                    continue;
+
+                if ((bool)dockViewElementObject.GetType().GetProperty("IsPinned").GetValue(dockViewElementObject))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    [Command(PackageIds.LoadTabsCommand)]
+    internal sealed class LoadTabsCommand : BaseCommand<LoadTabsCommand>
+    {
+        protected override void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE2 dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+
+            Solution solution = SaveFileManager.GetSavedSolution(dte.Solution.FullName);
+            if (solution == null)
+            {
+                VS.MessageBox.Show(
+                    "There are no tabs saved for this solution.",
+                    buttons: OLEMSGBUTTON.OLEMSGBUTTON_OK);
+
+                return;
+            }
+
+            foreach (Tab tab in solution.Tabs)
+            {
+                try
+                {
+                    ProjectItem proj = dte.Solution.FindProjectItem(tab.FullName);
+                    if (proj == null)
+                        continue;
+
+                    dte.Documents.Open(tab.FullName);
+
+                    if (tab.IsPinned)
+                        dte.ExecuteCommand("Window.PinTab");
+                }
+                catch (Exception)
+                {
+                    // failed documents
+                }
+            }
+        }
+    }
+}
---
+++ TabSaver/SolutionsSettings.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TabSaver
+{
+    internal class Tab(string fullName, bool isPinned)
+    {
+        public string FullName { get; set; } = fullName;
+        public bool IsPinned { get; set; } = isPinned;
+    }
+
+    internal class Solution(string fullName)
+    {
+        public string FullName { get; set; } = fullName;
+        public List<Tab> Tabs { get; set; } = [];
+    }
+
+    internal class SolutionsSettings
+    {
+        public List<Solution> Solutions { get; set; } = [];
+    }
+
+    internal static class SaveFileManager
+    {
+        private static string ExtensionFolder =>
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "TabSaverExtension");
+
+        private static string SolutionsSettingsFilePath =>
+            Path.Combine(ExtensionFolder, "SolutionsSettings.txt");
+
+        private static SolutionsSettings SolutionsSettings => GetSavedSolutionsSettings();
+
+        private static SolutionsSettings GetSavedSolutionsSettings()
+        {
+            if (File.Exists(SolutionsSettingsFilePath))
+            {
+                string loadedSolutionsSettings = File.ReadAllText(SolutionsSettingsFilePath);
+                return JsonConvert.DeserializeObject<SolutionsSettings>(loadedSolutionsSettings);
+            }
+            else
+            {
+                return new SolutionsSettings();
+            }
+        }
+
+        public static Solution GetSavedSolution(string solutionName)
+        {
+            if (string.IsNullOrWhiteSpace(solutionName))
+                return null;
+
+            return SolutionsSettings.Solutions.FirstOrDefault(s =>
+                string.Equals(s.FullName, solutionName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static void Save(Solution solution)
+        {
+            Directory.CreateDirectory(ExtensionFolder);
+
+            if (solution == null)
+                return;
+
+            SolutionsSettings currentSettings = SolutionsSettings;
+
+            int savedSolutionIndex =
+                currentSettings.Solutions.FindIndex(s =>
+                    string.Equals(s.FullName, solution.FullName, StringComparison.OrdinalIgnoreCase));
+
+            if (savedSolutionIndex != -1)
+            {
+                currentSettings.Solutions[savedSolutionIndex] = solution;
+            }
+            else
+            {
+                currentSettings.Solutions.Add(solution);
+            }
+
+            string serializedSaveFile = JsonConvert.SerializeObject(currentSettings, Formatting.Indented);
+            File.WriteAllText(SolutionsSettingsFilePath, serializedSaveFile);
+        }
+    }
+}
