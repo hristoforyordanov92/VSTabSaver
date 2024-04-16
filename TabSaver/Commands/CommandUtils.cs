9a74920efd531c932229f5cbd0533623c5a2bea5
Hristofor Yordanov <hristofor.yordanov.92@gmail.com>
2024-04-16 20:46:54 +0300

    Improve the UI and add a new command.

---
+++ TabSaver/Commands/CommandUtils.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using EnvDTE;
+
+namespace TabSaver
+{
+    internal static class CommandUtils
+    {
+        public static IEnumerable<Tab> ExtractAllOpenTabs(EnvDTE.Documents documents, bool pinnedOnly = false)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            foreach (Document document in documents)
+            {
+                Window activeWindow = document.ActiveWindow;
+
+                // some documents don't have a window. this happens with .xaml documents (maybe other types too).
+                // even when explicitly closed from the X button on the tab, they are still in the Documents collection
+                if (activeWindow == null)
+                    continue;
+
+                bool isPinned = IsDocumentPinned(document);
+
+                if (pinnedOnly && !isPinned)
+                    continue;
+
+                yield return new Tab(document.FullName, isPinned);
+            }
+        }
+
+        /// <summary>
+        /// Looks for a non-public property indicating if the document's window is pinned and returns the result.
+        /// </summary>
+        /// <param name="document">The document for which we want to know if it's pinned or not.</param>
+        /// <returns>True if the document is pinned, otherwise false.</returns>
+        public static bool IsDocumentPinned(Document document)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (document == null)
+                return false;
+
+            // some documents have multiple windows associated with them. idk why
+            // if we use document.ActiveWindow then we might end up with "IsPinned = false", because
+            // the DockViewElement is null for that specific window. so we go over each window
+            // and we get a true/false based on wether at least a single one is true, otherwise false
+            foreach (Window window in document.Windows)
+            {
+                if (window == null)
+                    continue;
+
+                PropertyInfo dockViewElementProperty = window
+                    .GetType()
+                    .GetProperty(
+                        "DockViewElement",
+                        BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty);
+
+                if (dockViewElementProperty == null)
+                    continue;
+
+                object dockViewElementObject = dockViewElementProperty.GetValue(window);
+                if (dockViewElementObject == null)
+                    continue;
+
+                if ((bool)dockViewElementObject.GetType().GetProperty("IsPinned").GetValue(dockViewElementObject))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
--- TabSaver/VSCommandTable.cs
+++ TabSaver/VSCommandTable.cs
@@ -12,16 +12,23 @@ namespace TabSaver
     /// </summary>
     internal sealed partial class PackageGuids
     {
-        public const string TabSaverString = "831c4fde-f595-496a-9b65-b5adce8ce935";
-        public static Guid TabSaver = new Guid(TabSaverString);
+        public const string TabSaverPackageString = "fe04de0a-2316-4e08-ab8b-7b1feaf83168";
+        public static Guid TabSaverPackage = new Guid(TabSaverPackageString);
+
+        public const string TabSaverCommandSetString = "31a662c6-2290-450a-846e-3b85c6fe17a2";
+        public static Guid TabSaverCommandSet = new Guid(TabSaverCommandSetString);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
     /// </summary>
     internal sealed partial class PackageIds
     {
-        public const int MyMenuGroup = 0x0001;
-        public const int SaveTabsCommand = 0x0100;
-        public const int LoadTabsCommand = 0x0101;
+        public const int TabSaverMenu = 0x1000;
+        public const int MainMenuTabSaverGroup = 0x0100;
+        public const int TabSaverCommandsGroup = 0x0200;
+        public const int SavePinnedTabsCommand = 0x0010;
+        public const int SaveAllTabsCommand = 0x0020;
+        public const int RestoreSavedTabsCommand = 0x0030;
+        public const int TestButton = 0x0040;
     }
 }
